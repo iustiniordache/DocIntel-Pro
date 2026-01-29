8819fbe62e0c0b8d7dc538fbac41c6ee6d892dcb
Iustin Iordache <iustin.iordache@moduscreate.com>
2026-01-29 14:10:42 +0200

    fix: standardize quote style in JavaScript files

--- packages/shared/src/types.js
+++ packages/shared/src/types.js
@@ -1,11 +1,11 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
 exports.DocumentStatus = void 0;
 var DocumentStatus;
 (function (DocumentStatus) {
-    DocumentStatus["PENDING"] = "PENDING";
-    DocumentStatus["PROCESSING"] = "PROCESSING";
-    DocumentStatus["COMPLETED"] = "COMPLETED";
-    DocumentStatus["FAILED"] = "FAILED";
+  DocumentStatus['PENDING'] = 'PENDING';
+  DocumentStatus['PROCESSING'] = 'PROCESSING';
+  DocumentStatus['COMPLETED'] = 'COMPLETED';
+  DocumentStatus['FAILED'] = 'FAILED';
 })(DocumentStatus || (exports.DocumentStatus = DocumentStatus = {}));
 //# sourceMappingURL=types.js.map
